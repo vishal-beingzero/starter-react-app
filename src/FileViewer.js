e8b0cab967802635142a094d5f22eaff478e2539
Vishal Kumar <96096248+vishal-kumar4@users.noreply.github.com>
2023-08-06 20:35:31 +0530

    used custom css

--- src/FileViewer.js
+++ src/FileViewer.js
@@ -3,7 +3,7 @@ import { BiShow, BiHide } from 'react-icons/bi';
 import { AiOutlinePieChart } from 'react-icons/ai';
 import { FaUpload, FaSearchengin } from 'react-icons/fa';
 import { Chart } from "react-google-charts";
-
+import './helper.css'
 const FileViewer = () => {
   const[fileContent, setFileContent] = useState('');
   const [fileName, setFileName] = useState('');
