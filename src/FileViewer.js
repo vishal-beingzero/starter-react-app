d7954b70836e762f910151d28bb48aa86c895677
Vishal Kumar <96096248+vishal-kumar4@users.noreply.github.com>
2023-08-06 20:28:48 +0530

    made major changes

--- src/FileViewer.js
+++ src/FileViewer.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
+import { BiShow, BiHide } from 'react-icons/bi';
+import { AiOutlinePieChart } from 'react-icons/ai';
+import { FaUpload, FaSearchengin } from 'react-icons/fa';
+import { Chart } from "react-google-charts";
+import './helper.css'
 
 const FileViewer = () => {
-  const [fileContent, setFileContent] = useState('');
+  const[fileContent, setFileContent] = useState('');
   const [fileName, setFileName] = useState('');
+  const [cv, setCV] = useState('');
   const [showData, setShowData] = useState(false);
   const [shoewcv, setshowcv] = useState(false);
   const [takeCv, setTakeCvs] = useState(false);
   const [disable, setDisable] = useState(false);
   const [loading, setLoading] = useState(false); // Added state variable for loading animation
+  
+  const [displayPie, setDisplayPie] = useState(false);
 
+  const [info, setInfo] = useState(false);
+  const [analysecv, setanalysecv] = useState(false);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -20,7 +30,7 @@ const FileViewer = () => {
       reader.onload = (e) => {
         const content = e.target.result;
         setFileContent(content);
-        setShowData(true);
+        // setShowData(true);
         setshowcv(true);
         setDisable(true)
         setLoading(false); // Hide loading animation once the file is loaded
@@ -39,13 +49,61 @@ const FileViewer = () => {
       setFileName('');
       setFileContent('');
       setShowData(false);
-      console.log("hiola  mfs")
+      setLoading(false); // Hide loading animation once the file is loaded
+
+    }
+  };
+  const handlepiechart=()=>{
+    setDisplayPie(true)
+    setanalysecv(false)
+  }
+
+  const analyseHandle = () => {
+    setanalysecv(true)
+    setDisplayPie(false)
+  }
+  const handleCV = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFileName(file.name);
+
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const content = e.target.result;
+        setCV(content);
+        // setShowData(true);
+        setInfo(true)
+
+      };
+
+      if (file.type === 'application/pdf' || file.type === 'text/plain') {
+        reader.readAsText(file);
+      } else {
+        alert('Please upload a PDF or TXT file.');
+        setLoading(false); // Hide loading animation once the file is loaded
+
+      }
+    } else {
+      // Reset the state when no file is selected
+      setCV('');
       setLoading(false); // Hide loading animation once the file is loaded
 
     }
   };
 
-  const handleTakeCV = () =>{
+  const data = [
+    ["Your skills", "Description equirements"],
+  ];
+  data.push(["nothing yet",100])
+
+  const options = {
+    title: "Pie Chart for tags based",
+    // is3D: true, //for 3d uncomment
+    pieHole: 0.4, //for donut chart uncomment
+  };
+
+  const handleTakeCV = () => {
     setShowData(false)
     setTakeCvs(true)
   }
@@ -55,59 +113,113 @@ const FileViewer = () => {
 
   return (
     <>
-    <figure class="text-center mt-3">
-  <blockquote class="blockquote">
-    <p>A Model that analyses provided <strong>Job description</strong> with your CV.</p>
-  </blockquote>
-  <figcaption class="blockquote-footer">
-    Sounds good <cite title="Source Title">isn't it ? </cite>
-  </figcaption>
-</figure>
-    <div className="container my-4">
-      <div className="card shadow">
-        <div className="card-header bg-primary text-white d-flex justify-content-between align-items-center">
-          <h4 className="mb-0">Job Description</h4>
-          
-        </div>
-        <div className="card-body">
-        <div className="alert alert-primary">
-              <strong>Note : </strong>Drag or Choose job description file to compare with differenet CV'S
+      <figure class="text-center mt-3">
+        <blockquote class="blockquote">
+          <p>A Model that analyses provided <strong>Job description</strong> with your CV.</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+          Sounds good <cite title="Source Title">isn't it ? </cite>
+        </figcaption>
+      </figure>
+      <div className="container my-4">
+        <div className="card shadow">
+          <div className="card-header bg-primary text-white d-flex justify-content-between align-items-center">
+            <h4 className="mb-0">Analyse my Cover Letter </h4>
           </div>
-          <div className="mb-3">
-            <input type="file" className="form-control" accept=".pdf,.txt" onChange={handleFileChange} disabled={disable}/>
-            {loading && <div className="text-center">Loading...</div>} {/* Display loading animation */}
+          <div className="card-body">
+            <div className="alert alert-primary">
+              {!disable && (
+                <>
+                  <strong>Note : </strong>Drag or Choose job description file .
+                </>
+              )}
+              {disable && (
+                <>
+                  <strong>Job Description </strong> has been uploaded sucessfully ,
+                  <strong>You Can now uplaod CV to Analyse</strong>
+                </>
+              )}
 
-          </div>
-
-          <button className="btn btn-primary" onClick={handleGetData}>
-            {!showData ? 'Show Description' : 'Hide Description' }
-          </button>
-          {shoewcv && 
-          (<button className="btn btn-primary mx-2" onClick={handleTakeCV}>
-            Upload CV
-          </button>
-          )}
-          <hr />
-          {showData && fileContent && (
-            <div>
-              <h5 className="mb-3 mt-2">A view of provided Job description</h5>
-              <pre className="border p-3 bg-light text-dark">
-                {fileContent}
-              </pre>
-              <div className="mt-3 text-center">
-              </div>
             </div>
-          )}
 
-          {takeCv && (          
-            <div className="mb-3 mt-2">
-            <input type="file" className="form-control" accept= ".pdf,.txt" onChange={handleFileChange} />
-          </div>) }
+            <div className="mb-3">
+              <input type="file" className="form-control" accept=".pdf,.txt" onChange={handleFileChange} disabled={disable} />
+              {loading && <div className="text-center">Loading...</div>} {/* Display loading animation */}
+            </div>
+
+            <button className="btn btn-primary" onClick={handleGetData}>
+              {!showData &&
+                <>
+                  <BiShow /> Show Description
+                </>
+              }
+              {showData &&
+                <>
+                  <BiHide /> Hide Description
+                </>
+              }
+
+            </button>
+            {shoewcv &&
+              (<button className="btn btn-primary mx-2" onClick={handleTakeCV}>
+                <FaUpload /> Upload CV
+              </button>
+              )}
+            {showData && fileContent && (
+              <>
+                <h5 className="mb-3 mt-2">A view of provided Job description</h5>
+                <div className="display-section">
+                  <pre className="border p-3 bg-light text-dark">{fileContent}</pre>
+                </div>
+              </>
+            )}
+
+            {takeCv && (
+              <>
+                <div className="mb-3 mt-3">
+                  <input type="file" className="form-control" accept=".pdf,.txt" onChange={handleCV} />
+                </div>
+                {!analysecv && (
+                <div className="alert alert-info">
+                  <strong>Drag and Drop</strong> or <strong>Select</strong> Your CV
+                </div>
+                )}
+              </>
+            )
+            }
+            {info && (
+              <>
+              <button className="btn btn-primary mx-2" onClick={analyseHandle}>
+                <FaSearchengin /> Analyse MY CV
+              </button>
+              <button className="btn btn-primary mx-2" onClick={handlepiechart}>
+                <AiOutlinePieChart /> Show Differences
+              </button>
+              </>
+            )}
+             {analysecv && cv && (
+              <>
+                <h5 className="mb-3 mt-2">A view of provided Job description</h5>
+                <div className="display-section">
+                  <pre className="border p-3 bg-light text-dark">{cv}</pre>
+                </div>
+              </>
+            )}
+            {displayPie && 
+                <Chart
+                chartType="PieChart"
+                data={data}
+                options={options}
+                width={"100%"}
+                height={"400px"}
+              />
+              }
+            
+          </div>
         </div>
       </div>
-    </div>
-            </>
+    </>
   );
-};
-
+;
+            }
 export default FileViewer;
